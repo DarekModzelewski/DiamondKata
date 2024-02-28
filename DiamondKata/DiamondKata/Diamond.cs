852bff016cb15e455a866b6e97d6b8401c528b7b
Dariusz Modzelewski <dariusz.modzelewski@fenergo.com>
2024-02-29 00:28:36 +0100

    [DM] addition of failing tests to the playground and ASCII table for the reference

---
+++ DiamondKata/DiamondKata/Diamond.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiamondKata
+{
+    public class Diamond
+    {
+        private const char FirstLetter = 'A';
+        private const char PaddingChar = ' ';
+        private const string LineSeparator = "\n";
+
+        public static string GenerateSingleLine(int paddingLeft, int character, int paddingMiddle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static List<string> GenerateHalfDiamond(char middleCharacter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string Create(char middleCharacter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
