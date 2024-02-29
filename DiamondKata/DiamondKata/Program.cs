a62750f152deecbb01f1a9dabc2146fc460d47ed
Dariusz Modzelewski <dariusz.modzelewski@fenergo.com>
2024-02-29 02:37:02 +0100

    [DM] added changes to Program.cs in order to consume Diamond class

--- DiamondKata/DiamondKata/Program.cs
+++ DiamondKata/DiamondKata/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DiamondKata;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        bool continueProgram = true;
+
+        while (continueProgram)
+        {
+            Console.Write("Enter a character: ");
+
+            char character = Console.ReadLine().ToUpper()[0];
+            var output = Diamond.Create(character);
+            Console.WriteLine(output);
+
+            Console.Write("Do you want to enter another character? (y/n): ");
+
+            char response = Console.ReadLine().ToLower()[0];
+
+            if (response != 'y')
+            {
+                continueProgram = false;
+            }
+        }
+
+
+    }
+
+}
